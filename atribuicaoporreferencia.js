0497a4003d97e8e6822b784a9c5ac66fd16001f1
Batalhone <51683231+batalhone@users.noreply.github.com>
2019-08-05 20:59:40 -0300

    Add files via upload

---
+++ atribuicaoporreferencia.js
@@ -0,0 +1,14 @@
+console.log('olá')
+const a = {name: 'teste'}
+console.log(a) 
+const b = a
+console.log(b)
+b.name = 'opa'
+console.log(a)
+console.log(b)
+
+let c = 3
+let d = c
+d++ 
+console.log(d)
+
---
+++ nullundefined.js
@@ -0,0 +1,18 @@
+let valor
+console.log(valor)
+
+valor = null
+
+console.log(valor)
+
+
+const produto = {}
+console.log(produto.preco)
+console.log(produto)
+produto.preco = 3.50
+console.log(produto)
+produto.preco = undefined
+console.log(produto.preco)
+console.log(!!produto.preco)
+produto.preco = null
+console.log(produto.preco)
