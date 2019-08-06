dd60bbf58319a8772d5abbfa15ac49a60d0df686
Batalhone <51683231+batalhone@users.noreply.github.com>
2019-08-06 14:25:55 -0300

    Add files via upload

---
+++ case1.js
@@ -0,0 +1,24 @@
+let dia;
+switch (new Date().getDay()) {
+  case 0:
+    dia = "Domingo";
+    break;
+  case 1:
+    dia = "Segunda-feira";
+    break;
+  case 2:
+    dia = "Terca-feira";
+    console.log('hoje é...'(dia))
+    break;
+  case 3:
+    dia = "Quarta-feira";
+    break;
+  case 4:
+    dia = "Quinta-feira";
+    break;
+  case 5:
+    dia = "Sexta-feira";
+    break;
+  case  6:
+    dia = "Sabado";
+}
